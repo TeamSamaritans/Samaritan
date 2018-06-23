02937585243632bf24cfcb50f6a7db0f9e6d5d23
Meenal Tiwari <meenal.tiwari@taritas.com>
2018-06-23 18:35:42 +0530

    Show web view page added

---
+++ Samaritan/Samaritan/Samaritan/View/ShowPost.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Samaritan.View
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ShowPost : ContentPage
+	{
+        private string postId;
+		public ShowPost(string id)
+		{
+			InitializeComponent ();
+            postId = id;
+		}
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            ShowPostView();
+        }
+
+        private void ShowPostView()
+        {
+            webview.Source = "http://taritas.in/left-behind/details-view/" + postId;
+        }
+    }
+}
