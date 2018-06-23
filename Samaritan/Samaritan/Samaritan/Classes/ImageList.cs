d1c7587fb06ae7ef08c88a1c73eccac373a4340a
KritikaGuruwani <KritikaGuruwani@DESKTOP-1QOV846>
2018-06-23 18:35:49 +0530

    added home page

---
+++ Samaritan/Samaritan/Samaritan/Classes/ImageList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Samaritan.Classes
+{
+   public class ImageList : BaseModel
+    {
+        public string id { get; set; }
+        public string user_id { get; set; }
+        public string image_src { get; set; }
+        public string longitude { get; set; }
+        public string latitude { get; set; }
+        public string craeted_at { get; set; }
+        public string updated_at { get; set; }
+    }
+}
